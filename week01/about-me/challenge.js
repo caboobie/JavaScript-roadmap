853c445f1c9b4084bf4a83cd3918d19d9951d093
caboobie <freedom-alex@hotmail.co.uk>
2026-07-26 17:42:22 +0100

    Challenge file about me

---
+++ week01/about-me/challenge.js
@@ -0,0 +1,10 @@
+const name = "Alexander Pickering";
+const age = "35"
+const job = "HGV Driver";
+const reason = "the haulage industry has let me down";
+const favourite = "how versitile it is";
+
+console.log(`Hi my name is ${name}.`);
+console.log(`I am ${age} years old.`);
+console.log(`my current job is ${job}, the reason im leaving it is because ${reason}.`);
+console.log(`My favourite thing about technology is ${favourite}.`);
