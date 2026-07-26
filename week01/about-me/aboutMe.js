586afca54a6e06f4c2850467b31b71877948cb87
caboobie <freedom-alex@hotmail.co.uk>
2026-07-26 14:41:20 +0100

    Create first JavaScript program

--- week01/about-me/aboutMe.js
+++ week01/about-me/aboutMe.js
@@ -2,6 +2,7 @@ const name = "Alexander Pickering";
 const goal = "To become a software developer";
 const hobbies = "Ice Hockey";
 
-console.log(name);
-console.log(goal);
-console.log(hobbies);
+console.log(`Hi, My name is ${name}.`);
+console.log(`My goal is ${goal}.`);
+console.log(`I am intrested in ${hobbies}.`);
+
