3bf88e816bcb008f4b7ab35fbb0ce886615305fd
caboobie <freedom-alex@hotmail.co.uk>
2026-07-26 18:24:43 +0100

    Profile exercise

---
+++ week01/about-me/profile.js
@@ -0,0 +1,13 @@
+const profile = {
+    name: "Alexander Pickering",
+    age: 35,
+    learningJavaScript: true,
+    sports: ["Ice Hockey"],
+    hobbies: ["Video games", "Video editting"]
+};
+
+console.log(profile.name);
+console.log(profile.age);
+console.log(profile.learningJavaScript);
+console.log(profile.sports);
+console.log(profile.hobbies);
