7628be7427ff533c086440ba484347a05133f842
caboobie <freedom-alex@hotmail.co.uk>
2026-07-26 18:50:55 +0100

    Player Card

---
+++ week01/hockey-player/player.js
@@ -0,0 +1,36 @@
+const player = {
+    name: "Alexander Pickering",
+    number: 90,
+    position: "Defence",
+    team: "Whitley Bay Sharks",
+    goals: 0,
+    assists: 1,
+    favouriteSharkFact: "Sharks first appeared 450 million years ago!"
+};
+
+const player2 = {
+    name: "Nathan Milne",
+    number: 21,
+    position: "centre",
+    team: "whitley Bay Sharks",
+    goals: 5,
+    assists: 8,
+    favouriteSharkFact: "sharks have existed longer than trees"
+};
+
+
+console.log(`Player: ${player.name}`);
+console.log(`Number: ${player.number}`);
+console.log(`Position: ${player.position}`);
+console.log(`Team: ${player.team}`);
+console.log(`Goals: ${player.goals}`);
+console.log(`Assists: ${player.assists}`);
+console.log(`Fact: ${player.favouriteSharkFact}`);
+console.log();
+console.log(`Player: ${player2.name}`);
+console.log(`Number: ${player2.number}`);
+console.log(`Position: ${player2.position}`);
+console.log(`Team: ${player2.team}`);
+console.log(`Goals: ${player2.goals}`);
+console.log(`Assists: ${player2.assists}`);
+console.log(`Fact: ${player2.favouriteSharkFact}`);
